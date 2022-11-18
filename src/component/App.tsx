14fbbb85f93d1ca8905e0c4ec3c3bdc58070d18b
soyunYang <yshshc@gmail.com>
2022-11-18 17:55:09 +0900

    update

---
+++ src/component/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+const MovableItem = () => {
+    return (
+        <div className='movable-item'>
+            We will move this item
+        </div>
+    )
+}
+
+const FirstColumn = () => {
+    return (
+        <div className='column first-column'>
+            Column 1
+            <MovableItem/>
+        </div>
+    )
+}
+
+const SecondColumn = () => {
+    return (
+        <div className='column second-column'>
+            Column 2
+        </div>
+    )
+}
+
+export const App = () => {
+    return (
+        <div className="container">
+            <FirstColumn/>
+            <SecondColumn/>
+        </div>
+    );
+}
